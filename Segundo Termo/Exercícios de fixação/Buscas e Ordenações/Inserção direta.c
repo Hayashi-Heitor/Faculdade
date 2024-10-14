4f2839832c94bc60657e55bc8d97d8bba0960e44
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2024-10-14 20:33:47 -0300

    Update Inserção direta.c

--- Segundo Termo/Exercícios de fixação/Buscas e Ordenações/Inserção direta.c
+++ Segundo Termo/Exercícios de fixação/Buscas e Ordenações/Inserção direta.c
@@ -2,16 +2,37 @@
 
 #define TF 20
 
+int BuscaBinaria(int TL, int vet[TF], int chave){
+	int inicio = 0, fim = TL-1, meio = TL/2;
+	
+	while(inicio < fim && chave != vet[meio]){
+		if(chave > vet[meio])
+			inicio = meio+1;
+		else
+			fim = meio-1;
+			
+		meio = (inicio+fim) / 2;
+	}
+	if(chave == vet[meio])
+		return meio;
+	else
+		return -1;
+}
+
 void InsercaoDireta(int TL, int vet[TF]){
 	int aux;
-	TL--;
-	while(TL > 0 && vet[TL] < vet[TL-1]){
-		aux = vet[TL];
-		vet[TL] = vet[TL-1];
-		vet[TL-1] = aux;
-		TL--;
-	}
 	
+	if(BuscaBinaria(TL, vet, vet[TL-1]) > 0 && TL > 2)
+		printf("Valor ja presente no vetor\n");
+		
+	else{
+		while(TL-1 > 0 && vet[TL-1] < vet[TL-2]){
+			aux = vet[TL-1];
+			vet[TL-1] = vet[TL-2];
+			vet[TL-2] = aux;
+			TL--;
+		}
+	}
 }
 
 int main (void){
