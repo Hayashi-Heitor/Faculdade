30adacfe29d4c02282e3333c9cf9111c043f77fe
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2024-11-08 20:43:22 -0300

    Update básico.c

--- Segundo Termo/Exercícios de fixação/Arquivo/básico.c
+++ Segundo Termo/Exercícios de fixação/Arquivo/básico.c
@@ -7,16 +7,34 @@
 
 struct TpAluno {
 	char RA[14], Nome[40];
-	int AnoNasc;
+	int AnoNasc, Status;
 };
 
 int BuscaAlunoRA(FILE *PtrAluno, char ChaveRA[14]) {
 	TpAluno R;
+	
 	//fseek(PtrAluno,0,0);
 	rewind(PtrAluno);
 	fread(&R,sizeof(TpAluno),1,PtrAluno);
 	
 	while (!feof(PtrAluno) && strcmp(ChaveRA,R.RA)!=0)
+		fread(&R,sizeof(TpAluno),1,PtrAluno);
+	
+	if (strcmp(ChaveRA,R.RA)==0)
+		return ftell(PtrAluno)-sizeof(TpAluno);
+	else
+		return -1;
+}
+
+int BuscaLogicaRA(FILE *PtrAluno, char ChaveRA[14]) {
+	TpAluno R;
+
+	rewind(PtrAluno);
+	fread(&R,sizeof(TpAluno),1,PtrAluno);
+	
+	//while(!feof(PtrAluno) && (strcmp(ChaveRA,R.RA) != 0 || R.Status == 0 ))
+	while (!feof(PtrAluno) && !(strcmp(ChaveRA,R.RA) == 0 && R.Status == 1))
+	
 		fread(&R,sizeof(TpAluno),1,PtrAluno);
 	
 	if (strcmp(ChaveRA,R.RA)==0)
@@ -42,6 +60,7 @@ void GravarAluno(void) {
 			gets(Reg.Nome);
 			printf("Ano de Nasc.:" );
 			scanf("%d",&Reg.AnoNasc);
+			Reg.Status = 1;
 			
 			fwrite(&Reg,sizeof(TpAluno),1,PtrAlu);
 			printf("\nDados Cadastrados!\n");
@@ -68,11 +87,13 @@ void ExibirAluno(void) {
 		printf("\nErro de Abertura!\n");
 	else {
 			fread(&R,sizeof(TpAluno),1,PtrAlu);
-			while (!feof(PtrAlu))
-			{
+			while (!feof(PtrAlu)) {
+				if(R.Status) {
 				printf("\nRA: %s",R.RA);
 				printf("\nNome: %s",R.Nome);
 				printf("\nAno Nasc.: %d\n",R.AnoNasc);
+				printf("\n%d", R.Status);
+				}
 				fread(&R,sizeof(TpAluno),1,PtrAlu);
 			}
 			fclose(PtrAlu);
@@ -95,13 +116,11 @@ void ConsultarAluno(void) {
 			printf("\nR.A. a Consultar: ");
 			fflush(stdin);
 			gets(RAluno.RA);
-			while(strcmp(RAluno.RA,"\0")!=0)
-			{
+			while(strcmp(RAluno.RA,"\0")!=0) {
 				pos = BuscaAlunoRA(PtrAlu,RAluno.RA);
 				if (pos == -1)
 					printf("\nR.A. Nao Cadastrado!\n");
-				else
-					{
+				else {
 						printf("\n*** Dados Encontrados ***\n");
 						fseek(PtrAlu,pos,0);
 						fread(&RAluno,sizeof(TpAluno),1,PtrAlu);
@@ -135,13 +154,11 @@ void AlterarAluno(void) {
 			printf("\nR.A. a Alterar: ");
 			fflush(stdin);
 			gets(RAluno.RA);
-			while(strcmp(RAluno.RA,"\0")!=0)
-			{
+			while(strcmp(RAluno.RA,"\0")!=0) {
 				pos = BuscaAlunoRA(PtrAlu,RAluno.RA);
 				if (pos == -1)
 					printf("\nR.A. Nao Cadastrado!\n");
-				else
-					{
+				else {
 						printf("\n*** Dados Encontrados ***\n");
 						fseek(PtrAlu,pos,0);
 						fread(&RAluno,sizeof(TpAluno),1,PtrAlu);
@@ -186,15 +203,13 @@ void OrdenarAluno(void) {
 			fseek(PtrAlu,0,2);
 			QtdeReg = ftell(PtrAlu)/sizeof(TpAluno);
 			for(a=0; a<QtdeReg-1; a++)
-				for(b=a+1; b<QtdeReg; b++)
-				{
+				for(b=a+1; b<QtdeReg; b++) {
 					fseek(PtrAlu,a*sizeof(TpAluno),0);
 					fread(&RegA,sizeof(TpAluno),1,PtrAlu);
 					
 					fseek(PtrAlu,b*sizeof(TpAluno),0);
 					fread(&RegB,sizeof(TpAluno),1,PtrAlu);
-					if (stricmp(RegA.Nome,RegB.Nome)>0)
-					{
+					if (stricmp(RegA.Nome,RegB.Nome)>0) {
 						fseek(PtrAlu,a*sizeof(TpAluno),0);
 						fwrite(&RegB,sizeof(TpAluno),1,PtrAlu);
 						
@@ -267,6 +282,93 @@ void ExclusaoFisica(void) {
 	getche();
 }
 
+void ExclusaoLogica(void) {
+	TpAluno RegAlu;
+	FILE *PtrAlu = fopen("Alunos.dat", "rb");
+	int pos;
+	char AuxRA[14];
+	
+	system("cls");
+	if(PtrAlu == NULL)
+		printf("\nErro de Abertura");
+		
+	else {
+		
+		printf("\nR.A a Excluir: ");
+		fflush(stdin);
+		gets(AuxRA);
+		pos = BuscaLogicaRA(PtrAlu, AuxRA);
+		
+		if (pos == -1) {
+			printf("\nAluno não cadastrado!\n");
+			fclose(PtrAlu);
+		}
+			
+		else {
+			
+			printf("\nDados do aluno:\n");
+			fseek(PtrAlu, pos, 0);
+			fread(&RegAlu, sizeof(TpAluno), 1, PtrAlu);
+			printf("R.A: %s\n", RegAlu.RA);
+			printf("Nome: %s\n", RegAlu.Nome);
+			printf("Ano de Nasc: %d\n", RegAlu.AnoNasc);
+			printf("\nConfirmar a exclusão do aluno? (S/N) ");
+			
+			if(toupper(getche()) == 'S') {
+				
+				RegAlu.Status = 0;
+				fseek(PtrAlu, pos, 0);
+				fwrite(&RegAlu,sizeof(TpAluno),1,PtrAlu);
+				printf("\nExclusão realizada com sucesso!\n");
+
+			}
+
+		}
+		fclose(PtrAlu);
+	}
+	getche();
+}
+
+void ExclusaoFisicaTodos(void) {
+	TpAluno RegAlu;
+	FILE *PtrAlu = fopen("Alunos.dat", "rb");
+	
+	system("cls");
+	if(PtrAlu == NULL)
+		printf("\nErro de Abertura");
+		
+	else {
+	
+		printf("\nDados do aluno:\n");
+		fseek(PtrAlu, pos, 0);
+		fread(&RegAlu, sizeof(TpAluno), 1, PtrAlu);
+		printf("R.A: %s\n", RegAlu.RA);
+		printf("Nome: %s\n", RegAlu.Nome);
+		printf("Ano de Nasc: %d\n", RegAlu.AnoNasc);
+		printf("\nConfirmar a exclusão do aluno? (S/N) ");
+		
+		if(toupper(getche()) == 'S') {
+			
+			FILE *PtrTemp = fopen("Temp.dat", "wb");
+			rewind(PtrAlu);
+			fread(&RegAlu, sizeof(TpAluno), 1, PtrAlu);
+			
+			while(!feof(PtrAlu)) {
+				if(RegAlu.Status)
+					fwrite(&RegAlu, sizeof(TpAluno), 1, PtrTemp);
+				
+				fread(&RegAlu, sizeof(TpAluno), 1, PtrAlu);
+			}
+			fclose(PtrAlu);
+			fclose(PtrTemp);
+			remove("Alunos.dat");
+			rename("Temp.dat", "Alunos.dat");
+			printf("\nExclusão realizada com sucesso!\n");
+		}
+	}
+	getche();
+}
+
 char Menu(void) {
 	
 	clrscr();
@@ -276,9 +378,11 @@ char Menu(void) {
 	printf("\n[C] Consultar por R.A.");
 	printf("\n[D] Alterar por R.A.");
 	printf("\n[E] Ordenar por R.A.");
-	printf("\n[F] Exclusão por R.A");
+	printf("\n[F] Exclusão (fisica) por R.A");
+	printf("\n[G] Exclusão (lógica) por R.A");
+	printf("\n[H] Exclusão física de todos os alunos excluidos lógicamente");
 	printf("\n[ESC] Finalizar");
-	printf("\nOpcao desejada: ");
+	printf("\n\nOpcao desejada: ");
 	return toupper(getche());
 }
 
@@ -286,11 +390,9 @@ int main(void) {
 	char opcao;
 	
 	setlocale(LC_ALL, "portuguese");
-	do
-	{
+	do {
 		opcao = Menu();
-		switch(opcao)
-		{
+		switch(opcao) {
 			case 'A':	GravarAluno();
 						break;
 			case 'B':	ExibirAluno();
@@ -302,13 +404,13 @@ int main(void) {
 			case 'E':	OrdenarAluno();
 						break;
 			case 'F':	ExclusaoFisica();
-						break; 	
+						break;
+			case 'G':	ExclusaoLogica();
+						break;
+			case 'H':	ExclusaoFisicaTodos();
+						break;
 		}
 	}while (opcao!=27);
 	
 	return 0;
 }
-
-
-
-
