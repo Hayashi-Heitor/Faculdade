1e1835a03735d1be517dd3e1436c6cec8701daf9
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2025-02-10 22:12:05 -0300

    Update Programa.cpp

--- Terceiro Termo/Estrutura de Dados I/Headers/Programa.cpp
+++ Terceiro Termo/Estrutura de Dados I/Headers/Programa.cpp
@@ -3,20 +3,52 @@
 #include <conio2.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <>
+#include <locale.h>
 
 #include "TADpopulacao.h"
 
-void Executar() {
-	
+char Menu() {
+	clrscr();
+	printf("\n### M E N U ###\n");
+	printf("[A] Cadastrar Cidades\n");
+	printf("[B] Consultar Cidades pelo Nome\n");
+	printf("[C] Ordenar Cidades pelo Nome\n");
+	printf("[D] Ordenar Cidades pela População\n");
+	printf("[ESC] Finalizar\n");
+	printf("\nOpção Desejada: ");
+	return toupper(getch());
 }
 
-char Menu() {
+void Executar() {
+	char opcao;
 	
+	do {
+		opcao = Menu();
+		switch (opcao) {
+			
+			case 'A':
+				clrscr();
+				Cadastrar();
+				break;
+			
+			case'B':
+				clrscr();
+				Consultar();
+				break;
+				
+			case'C':
+				clrscr();
+				Ordenar();		
+		}
+		
+	}while(opcao != 27);
 }
+
+
 
 int main() {
 	
+	setlocale(LC_ALL, "portuguese");
 	Executar();
 	
 	return 0;
