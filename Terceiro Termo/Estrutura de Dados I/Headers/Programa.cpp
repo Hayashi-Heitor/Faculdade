d8e7ca9a6b6e76eff2f9c1cb23aceb97cedcbe3d
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2025-02-10 21:52:50 -0300

    Create Programa.cpp

---
+++ Terceiro Termo/Estrutura de Dados I/Headers/Programa.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio2.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <>
+
+#include "TADpopulacao.h"
+
+void Executar() {
+	
+}
+
+char Menu() {
+	
+}
+
+int main() {
+	
+	Executar();
+	
+	return 0;
+}
