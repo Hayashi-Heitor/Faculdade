68a4d4f83ac18ee2fad6e6a2b64e9ee71efd8b74
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2025-02-12 22:01:03 -0300

    Update EditorTexto.cpp

--- Terceiro Termo/Estrutura de Dados I/Editor de Texto/EditorTexto.cpp
+++ Terceiro Termo/Estrutura de Dados I/Editor de Texto/EditorTexto.cpp
@@ -8,6 +8,13 @@
 
 #define TF 70
 
+//Tamanhos Físicos relacionados a interface
+#define LARGURA_TELA_TEXTO 70
+#define ALTURA_TELA_TEXTO 25
+
+#define LARGURA_TELA_OPCOES 70
+#define ALTURA_TELA_OPCOES 3
+
 void VerificaArquivo(char NomeArquivo[TF]) {
 	FILE *PtrArq = fopen(NomeArquivo, "r");
 	
@@ -129,15 +136,65 @@ void ExibeTextoStr(char NomeArquivo[TF]) {
 	
 }
 
+//Interface
+
+void Moldura(int ColunaInicial, int LinhaInicial, int ColunaFinal, int LinhaFinal,int CorLinha,int CorFundo){
+	
+	textcolor(CorLinha);
+	textbackground(CorFundo);
+	gotoxy(ColunaInicial, LinhaInicial); printf("%c", 201);
+	gotoxy(ColunaFinal, LinhaInicial); printf("%c", 187);
+	gotoxy(ColunaInicial, LinhaFinal); printf("%c", 200);
+	gotoxy(ColunaFinal, LinhaFinal); printf("%c", 188);
+	
+	for(int i = ColunaInicial+1; i < ColunaFinal; i++){
+		gotoxy(i,LinhaInicial); printf("%c", 205);
+		gotoxy(i,LinhaFinal); printf("%c", 205);
+	}
+	for(int i = LinhaInicial+1; i < LinhaFinal; i++){
+		gotoxy(ColunaInicial,i); printf("%c", 186);
+		gotoxy(ColunaFinal,i); printf("%c", 186);
+	}
+	textcolor(7);
+	textbackground(0);
+	
+}
+
+void DesenhaInterface(void) {
+	
+	system("cls");
+	Moldura(5, 1, LARGURA_TELA_OPCOES, ALTURA_TELA_OPCOES, WHITE, 0);
+	gotoxy(8, 2); printf("SALVAR ARQUIVO");
+	gotoxy(30, 2); printf("RETORNAR AO MENU");
+	gotoxy(LARGURA_TELA_TEXTO/2 - 9, 6);
+	printf("---EDITOR DE TEXTO---");
+	Moldura(5, 7, LARGURA_TELA_TEXTO, ALTURA_TELA_TEXTO, WHITE, 0);
+	
+}
+
+void NavegaMenu(void) {
+	int OpcaoSelecionada
+	
+	if(OpcaoSelecionada == 1) {
+		
+	}
+	
+	if(OpcaoSelecionada == 2) {
+		
+	}
+	
+}
+
 int main (void) {
 	char Arquivo[TF];
 	
-	setlocale(LC_ALL, "portuguese");
-	LerNome(Arquivo);
+	//setlocale(LC_ALL, "portuguese");
+	/*LerNome(Arquivo);
 	VerificaArquivo(Arquivo);
 	LeTextoStr(Arquivo);
-	ExibeTextoStr(Arquivo);
-	
+	ExibeTextoStr(Arquivo);*/
+	DesenhaInterface();
+	printf("\n\n");
 	return 0;
 	
 }
